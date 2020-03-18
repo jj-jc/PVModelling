dc380629bb8eda54b22352a69995b66d7e2bf6ea
Juan José <juanjojuradocaminod.v@gmail.com>
2020-03-18 12:44:52 +0100

    Se ha corregido el error del tz en la función para obtener el la posición solar real.

--- Datos.py
+++ Datos.py
@@ -21,78 +21,150 @@ CPV_location=pvlib.location.Location(latitude=lat,longitude=lon,tz=tz,altitude=a
 #definicion del modulo, en principio no hace falta ya que no se va a usar el modelo SAPM
 # Modulo=pvlib.modelchain.ModelChain(system=Sistema, location=CPV_location, dc_model='cec')
 #Calculamos la posicion solar
-Solar_position=CPV_location.get_solarposition(times=df['Date Time'], pressure=None, temperature=df['T_Amb (°C)'])
+Fecha=pd.DatetimeIndex(df['Date Time'],tz=tz)
+
+Solar_position=CPV_location.get_solarposition(Fecha, pressure=None, temperature=df['T_Amb (°C)'])
 AOI=pd.DataFrame(pvlib.irradiance.aoi(surface_tilt=surface_tilt, surface_azimuth=surface_azimuth, 
                                       solar_zenith=Solar_position['zenith'], solar_azimuth=Solar_position['azimuth']))
-AM=CPV_location.get_airmass(times=df['Date Time'], solar_position=Solar_position)
+AM=CPV_location.get_airmass(times=Fecha, solar_position=Solar_position)
+
+
 ISC_DII=pd.Series(df['ISC_measured_IIIV (A)']/df['DII (W/m2)'])
+#la sentencia anterior puede generar valores infinitos, no provocan errores. Se filtrarán más adelante.
 AM=AM.set_index([pd.Series(df.index)])
 AOI=AOI.set_index([pd.Series(df.index)])
-
-
-
-
-
-#ISC_DII=ISC_DII.set_index([pd.Series(df.index)])
 df=pd.concat([df,AM,AOI,ISC_DII],axis=1)
 df=df.rename(columns={0:'ISC/DII'})
-#Criterios de filtrado para datos del III-V
-#Potencia estimada <0.001 ( un valor tan bajo no aporta información, de hecho puede empeorar el estudio)
-#angulo de incidencia >50 (Traking range)
-#criterios de Marcos, SMR, DNI,AM,viento
-
-filt_df=df[(df['PMP_estimated_IIIV (W)']>0.001)]
-filt_df=filt_df[(filt_df['aoi']<90.00)]
-#En la base de datos estaba con dos espacios"
-filt_df=filt_df[(filt_df['SMR_Top_Mid (n.d.)']!='   NaN')]
-filt_df[['SMR_Top_Mid (n.d.)']] = filt_df[['SMR_Top_Mid (n.d.)']].apply(pd.to_numeric)
-filt_df=filt_df[(filt_df['SMR_Top_Mid (n.d.)']<1.10)]
-filt_df=filt_df[(filt_df['SMR_Top_Mid (n.d.)']>0.7)]
-filt_df=filt_df[(filt_df['DII (W/m2)']>600.00)]
-filt_df=filt_df[(filt_df['Wind Speed (m/s)']<10.00)]
-filt_df=filt_df[(filt_df['airmass_relative']<10.00)]
-filt_df=filt_df.set_index(np.arange(0,len(filt_df)))
-
-'''vemos los datos representados tras el primer filtrado'''
-
-
-'''Este es el código para dibujar la nube de puntos con el filtrado'''
-x=filt_df['aoi']
-y1=filt_df['ISC/DII']
-x_aoi=filt_df['aoi']
-x_temp=filt_df['T_Amb (°C)']
-x_AM=filt_df['airmass_relative']
-
-#AOI
-fig, ax=plt.subplots(figsize=(10,7))
-ax.plot(x_aoi,y1,'o',markersize=2)
-#plt.ylim(0,0.0015)
-ax.set_xlabel('AOI (°)')
-ax.set_ylabel('ISC_measured_IIIV/DII (A m2/W)')
-ax.set_title("Datos")
+#Representamos el aoi y el airmass en función del tiempo en un día
+df=df.set_index(Fecha)
+df=df.drop(['Date Time'],axis=1)
+segundos=np.arange(0,10586)
+aoi=np.arange(0,10586)
+am=np.arange(0,10586)
+j=0
+for i in df.index[:]:
+    if(i.day==10):
+        segundos[j]=i.hour*3600+i.minute*60+i.second
+        aoi[j]=df.loc[i]['aoi']
+        am[j]=df.loc[i]['airmass_relative']
+        j=j+1
+        
+plt.figure(figsize=(10,7))
+plt.plot(segundos,aoi,'o',markersize=2,label='aoi en un día')
+plt.plot(segundos,am,'o',markersize=2,label='aimass relative en un día')
 plt.legend()
-#T_Amb
-fig, ax=plt.subplots(figsize=(10,7))
-ax.plot(x_temp,y1,'o',markersize=2)
-#ax.ylimit(0,0.0015)
-ax.set_xlabel('T_Amb (°C)')
-ax.set_ylabel('ISC_measured_IIIV/DII (A m2/W)')
-ax.set_title("Datos")
-plt.legend()
-#airmass_relative
-fig, ax=plt.subplots(figsize=(10,7))
-ax.plot(x_AM,y1,'o',markersize=2)
-#ax.ylimit(0,0.0015)
-ax.set_xlabel('airmass_relative')
-ax.set_ylabel('ISC_measured_IIIV/DII (A m2/W)')
-ax.set_title("Datos")
-plt.legend()
+plt.show()
 
+
+
+
+
+
+
+#
+###Criterios de filtrado para datos del III-V
+###Potencia estimada <0.001 ( un valor tan bajo no aporta información, de hecho puede empeorar el estudio)
+###angulo de incidencia >50 (Traking range)
+###criterios de Marcos, SMR, DNI,AM,viento
+##
+#filt_df=df[(df['PMP_estimated_IIIV (W)']>0.001)]
+#filt_df=filt_df[(filt_df['aoi']<90.00)]
+##En la base de datos estaba con dos espacios"
+#filt_df=filt_df[(filt_df['SMR_Top_Mid (n.d.)']!='   NaN')]
+#filt_df[['SMR_Top_Mid (n.d.)']] = filt_df[['SMR_Top_Mid (n.d.)']].apply(pd.to_numeric)
+#filt_df=filt_df[(filt_df['SMR_Top_Mid (n.d.)']<1.10)]
+#filt_df=filt_df[(filt_df['SMR_Top_Mid (n.d.)']>0.7)]
+#filt_df=filt_df[(filt_df['DII (W/m2)']>600.00)]
+#filt_df=filt_df[(filt_df['Wind Speed (m/s)']<10.00)]
+#filt_df=filt_df[(filt_df['airmass_relative']<10.00)]
+#filt_df=filt_df.set_index(np.arange(0,len(filt_df)))
+#
+#'''vemos los datos representados tras el primer filtrado'''
+#
+#
+#'''Este es el código para dibujar la nube de puntos con el filtrado'''
+#x=filt_df['aoi']
+#y1=filt_df['ISC/DII']
+#x_aoi=filt_df['aoi']
+#x_temp=filt_df['T_Amb (°C)']
+#x_AM=filt_df['airmass_relative']
+#
+##AOI
+#fig, ax=plt.subplots(figsize=(10,7))
+#ax.plot(x_aoi,y1,'o',markersize=2)
+##plt.ylim(0,0.0015)
+#ax.set_xlabel('AOI (°)')
+#ax.set_ylabel('ISC_measured_IIIV/DII (A m2/W)')
+#ax.set_title("Datos")
+#plt.legend()
+##T_Amb
+#fig, ax=plt.subplots(figsize=(10,7))
+#ax.plot(x_temp,y1,'o',markersize=2)
+##ax.ylimit(0,0.0015)
+#ax.set_xlabel('T_Amb (°C)')
+#ax.set_ylabel('ISC_measured_IIIV/DII (A m2/W)')
+#ax.set_title("Datos")
+#plt.legend()
+##airmass_relative
+#fig, ax=plt.subplots(figsize=(10,7))
+#ax.plot(x_AM,y1,'o',markersize=2)
+##ax.ylimit(0,0.0015)
+#ax.set_xlabel('airmass_relative')
+#ax.set_ylabel('ISC_measured_IIIV/DII (A m2/W)')
+#ax.set_title("Datos")
+#plt.legend()
+#
+#'''Hay que mejorar el filtrado'''
+#
+#
+#filt_df=filt_df[(filt_df['ISC/DII']<0.0012)]
+#filt_df=filt_df[(filt_df['ISC/DII']>0.0004)]
+#media_ISCDII=filt_df['ISC/DII'].mean()
+#limit_sup=media_ISCDII+0.0003
+#limit_inf=media_ISCDII-0.0003
+#filt_df=filt_df[filt_df['ISC/DII']>limit_inf]
+#filt_df=filt_df[filt_df['ISC/DII']<limit_sup]
+#
+#
+#
+#
+#'''Este es el código para dibujar la nube de puntos con el filtrado'''
+#x=filt_df['aoi']
+#y1=filt_df['ISC/DII']
+#x_aoi=filt_df['aoi']
+#x_temp=filt_df['T_Amb (°C)']
+#x_AM=filt_df['airmass_relative']
+#
+#
+##AOI
+#fig, ax=plt.subplots(figsize=(10,7))
+#ax.plot(x_aoi,y1,'o',markersize=2)
+#ax.set_ylim(0,0.0015)
+#ax.set_xlabel('AOI (°)')
+#ax.set_ylabel('ISC_measured_IIIV/DII (A m2/W)')
+#ax.set_title("Datos")
+#plt.legend()
+##T_Amb
+#fig, ax=plt.subplots(figsize=(10,7))
+#ax.plot(x_temp,y1,'o',markersize=2)
+#ax.set_ylim(0,0.0015)
+#ax.set_xlabel('T_Amb (°C)')
+#ax.set_ylabel('ISC_measured_IIIV/DII (A m2/W)')
+#ax.set_title("Datos")
+#plt.legend()
+##airmass_relative
+#fig, ax=plt.subplots(figsize=(10,7))
+#ax.plot(x_AM,y1,'o',markersize=2)
+#ax.set_ylim(0,0.0015)
+#ax.set_xlabel('airmass_relative')
+#ax.set_ylabel('ISC_measured_IIIV/DII (A m2/W)')
+#ax.set_title("Datos")
+#plt.legend()
 #
 #'''Potencias'''
 #
 #y1=filt_df['PMP_estimated_IIIV (W)']
-#y2=filt_df['PMP_estimated_Si (W)']
+##y2=filt_df['PMP_estimated_Si (W)']
 #x_aoi=filt_df['aoi']
 #x_temp=filt_df['T_Amb (°C)']
 #x_AM=filt_df['airmass_relative']
@@ -100,7 +172,7 @@ plt.legend()
 ##Potencia en función del aoi
 #fig, ax=plt.subplots(figsize=(10,7))
 #ax.plot(x_aoi,y1,'o',markersize=2,label='PMP_estimated_IIIV (W)')
-#ax.plot(x_aoi,y2,'o',markersize=2,label='PMP_estimated_Si (W)')
+##ax.plot(x_aoi,y2,'o',markersize=2,label='PMP_estimated_Si (W)')
 ##ax.ylimit(0,0.0015)
 #ax.set_xlabel('AOI (°)')
 #ax.set_ylabel('Potencia (W)')
@@ -108,119 +180,30 @@ plt.legend()
 #plt.legend()
 #
 #
-#
+##Potencia en función de la temp
 #fig, ax=plt.subplots(figsize=(10,7))
 #ax.plot(x_temp,y1,'o',markersize=2,label='PMP_estimated_IIIV (W)')
-#ax.plot(x_temp,y2,'o',markersize=2,label='PMP_estimated_Si (W)')
+##ax.plot(x_temp,y2,'o',markersize=2,label='PMP_estimated_Si (W)')
 ##ax.ylimit(0,0.0015)
 #ax.set_xlabel('Temp (°C)')
 #ax.set_ylabel('Potencia (W)')
 #ax.set_title("Potencias en función de Temp")
 #plt.legend()
 #
-#
+##Potencia en función del airmass
 #fig, ax=plt.subplots(figsize=(10,7))
 #ax.plot(x_AM,y1,'o',markersize=2,label='PMP_estimated_IIIV (W)')
-#ax.plot(x_AM,y2,'o',markersize=2,label='PMP_estimated_Si (W)')
+##ax.plot(x_AM,y2,'o',markersize=2,label='PMP_estimated_Si (W)')
 ##ax.ylimit(0,0.0015)
 #ax.set_xlabel('Airmass')
 #ax.set_ylabel('Potencia (W)')
 #ax.set_title("Potencias en función de Airmass")
 #plt.legend()
-
-
-
-
-'''Hay que mejorar el filtrado'''
-
-
-filt_df=filt_df[(filt_df['ISC/DII']<0.0012)]
-filt_df=filt_df[(filt_df['ISC/DII']>0.0004)]
-media_ISCDII=filt_df['ISC/DII'].mean()
-limit_sup=media_ISCDII+0.0003
-limit_inf=media_ISCDII-0.0003
-filt_df=filt_df[filt_df['ISC/DII']>limit_inf]
-filt_df=filt_df[filt_df['ISC/DII']<limit_sup]
-
-
-
-
-'''Este es el código para dibujar la nube de puntos con el filtrado'''
-x=filt_df['aoi']
-y1=filt_df['ISC/DII']
-x_aoi=filt_df['aoi']
-x_temp=filt_df['T_Amb (°C)']
-x_AM=filt_df['airmass_relative']
-
-
-#AOI
-fig, ax=plt.subplots(figsize=(10,7))
-ax.plot(x_aoi,y1,'o',markersize=2)
-ax.set_ylim(0,0.0015)
-ax.set_xlabel('AOI (°)')
-ax.set_ylabel('ISC_measured_IIIV/DII (A m2/W)')
-ax.set_title("Datos")
-plt.legend()
-#T_Amb
-fig, ax=plt.subplots(figsize=(10,7))
-ax.plot(x_temp,y1,'o',markersize=2)
-ax.set_ylim(0,0.0015)
-ax.set_xlabel('T_Amb (°C)')
-ax.set_ylabel('ISC_measured_IIIV/DII (A m2/W)')
-ax.set_title("Datos")
-plt.legend()
-#airmass_relative
-fig, ax=plt.subplots(figsize=(10,7))
-ax.plot(x_AM,y1,'o',markersize=2)
-ax.set_ylim(0,0.0015)
-ax.set_xlabel('airmass_relative')
-ax.set_ylabel('ISC_measured_IIIV/DII (A m2/W)')
-ax.set_title("Datos")
-plt.legend()
-
-'''Potencias'''
-
-y1=filt_df['PMP_estimated_IIIV (W)']
-#y2=filt_df['PMP_estimated_Si (W)']
-x_aoi=filt_df['aoi']
-x_temp=filt_df['T_Amb (°C)']
-x_AM=filt_df['airmass_relative']
-
-#Potencia en función del aoi
-fig, ax=plt.subplots(figsize=(10,7))
-ax.plot(x_aoi,y1,'o',markersize=2,label='PMP_estimated_IIIV (W)')
-#ax.plot(x_aoi,y2,'o',markersize=2,label='PMP_estimated_Si (W)')
-#ax.ylimit(0,0.0015)
-ax.set_xlabel('AOI (°)')
-ax.set_ylabel('Potencia (W)')
-ax.set_title("Potencias en función de AOI")
-plt.legend()
-
-
-#Potencia en función de la temp
-fig, ax=plt.subplots(figsize=(10,7))
-ax.plot(x_temp,y1,'o',markersize=2,label='PMP_estimated_IIIV (W)')
-#ax.plot(x_temp,y2,'o',markersize=2,label='PMP_estimated_Si (W)')
-#ax.ylimit(0,0.0015)
-ax.set_xlabel('Temp (°C)')
-ax.set_ylabel('Potencia (W)')
-ax.set_title("Potencias en función de Temp")
-plt.legend()
-
-#Potencia en función del airmass
-fig, ax=plt.subplots(figsize=(10,7))
-ax.plot(x_AM,y1,'o',markersize=2,label='PMP_estimated_IIIV (W)')
-#ax.plot(x_AM,y2,'o',markersize=2,label='PMP_estimated_Si (W)')
-#ax.ylimit(0,0.0015)
-ax.set_xlabel('Airmass')
-ax.set_ylabel('Potencia (W)')
-ax.set_title("Potencias en función de Airmass")
-plt.legend()
-
-'''decido crear otro excel con los datos ya filtrados, para mayor comodidad'''
-
-filt_df.to_excel("C://Users/juanj/OneDrive/Escritorio/TFG/Datos_filtrados.xlsx")
-
+#
+##'''decido crear otro excel con los datos ya filtrados, para mayor comodidad'''
+##
+##filt_df.to_excel("C://Users/juanj/OneDrive/Escritorio/TFG/Datos_filtrados.xlsx")
+##
 
 
 
